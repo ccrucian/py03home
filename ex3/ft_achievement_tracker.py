8ab72d31e7ad37a430261ad4a739277f0c0c1733
Carlotta Cruciani <ccrucian@c2r6s8.42roma.it>
2026-09-16 13:13:38 +0200

    ok

--- ex3/ft_achievement_tracker.py
+++ ex3/ft_achievement_tracker.py
@@ -1,7 +1,7 @@
 import random
 
 
-ALL_ACH: list[str] = [
+all_ach: list[str] = [
                         "Winner", "Loser", "Hunter", "King",
                         "Wizard", "Main", "Zero", "Capitain",
                         "Teacher", "Mask", "Ready", "Contig",
@@ -11,7 +11,7 @@ ALL_ACH: list[str] = [
 
 def gen_player_achievement() -> set[str]:
     number: int = random.randint(5, 10)
-    pick_randoms: list[str] = random.sample(ALL_ACH, number)
+    pick_randoms: list[str] = random.sample(all_ach, number)
     return set(pick_randoms)
 
 
@@ -22,14 +22,32 @@ players: dict[str, set[str]] = {
                         "Dylan": gen_player_achievement()
                             }
 
-    
+
 def main() -> None:
     print("=== Achievement Tracker System ===")
     for player, achievement in players.items():
         print(
             f"Player {player}: {achievement}"
             )
-    print(f"All distinct achievement: {set(ALL_ACH)}")
+    l_set: set[str] = set()
+    for v in players.values():
+        l_set = l_set.union(v)
+    print(f"All distinct achievement: {l_set}")
+    print()
+    # for player in players:
+    #    common: set[str] = l_set.intersection(players[player])
+    common: set[str] = l_set.intersection(*(players[i] for i in players))
+    print(f"Common: {common}")
+    print("")
+    for name, val in players.items():
+        unique: set[str] = val.difference(*(
+            players[other] for other in players if other != name
+            ))
+        print(f"Only {name} has: {unique}")
+    print()
+    for player in players:
+        missing: set[str] = set(all_ach).difference(players[player])
+        print(f"{player} is missing: {missing}")
 
 
 if __name__ == "__main__":
